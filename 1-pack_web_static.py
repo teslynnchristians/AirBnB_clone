966f10649e4a3ca720e8e437e7e8b9d212bce22f
teslynnchristians <104847651+teslynnchristians@users.noreply.github.com>
2022-12-06 13:08:57 +0200

    AirBnB clone - MySQL

---
+++ 1-pack_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack
+"""
+
+from os import path
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """
+    A function that generates an archive
+    """
+    date = datetime.now()
+    archive = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        date.year, date.month, date.day,
+        date.hour, date.minute, date.second
+    )
+    if not path.isdir("versions"):
+        if local("mkdir versions").failed:
+            return None
+    cmd = "cd web_static && tar -cvzf ../{} . && cd -".format(archive)
+    if local(cmd).succeeded:
+        return archive
+    return None
